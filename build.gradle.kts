da4d1f7f4dbe9b279d43a988990f1a5856fc9e40
Sid <sidgowda.9@gmail.com>
2023-02-06 20:30:37 -0700

    Added plugins and changed to Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,8 @@
  */
 
 // Root build.gradle.kts
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
