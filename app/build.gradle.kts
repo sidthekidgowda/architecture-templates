d2f6fbb9b69c7f5651725e348ab60f1c425f61b2
Sid <sidgowda.9@gmail.com>
2023-06-26 02:06:05 -0700

    added comment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ android {
         }
     }
 
+    // Make sure JDK is set to 17
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
